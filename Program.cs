0241a3b1ced24d8ef2c8c3857d03d47caebbe285
もちおいしい <150530030+Mochioishi@users.noreply.github.com>
2026-01-15 10:05:25 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,8 +12,8 @@ var builder = Host.CreateApplicationBuilder(args);
 // --- 1. Discord Socket Client の設定 ---
 builder.Services.AddSingleton(new DiscordSocketConfig
 {
-    // スラッシュコマンドには最低限以下が必要
-    GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers,
+    // メッセージ内容を読み取るために MessageContent を追加
+    GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers | GatewayIntents.MessageContent,
     AlwaysDownloadUsers = true,
     LogGatewayIntentWarnings = false
 });
@@ -23,24 +23,29 @@ builder.Services.AddSingleton<DiscordSocketClient>();
 builder.Services.AddSingleton<InteractionService>();
 builder.Services.AddSingleton<InteractionHandler>();
 
-// データベース設定 (GitHubの既存クラス)
 builder.Services.AddSingleton<DbConfig>();
 builder.Services.AddSingleton<DbInitializer>();
 
-// バックグラウンドサービス (Botの起動管理と時報)
 builder.Services.AddHostedService<Worker>();
 builder.Services.AddHostedService<TimeSignalWorker>();
 
 var host = builder.Build();
 
-// --- 3. データベースの初期化実行 ---
+// --- 3. データベースの初期化とグローバルコマンドの掃除 ---
 using (var scope = host.Services.CreateScope())
 {
     var initializer = scope.ServiceProvider.GetRequiredService<DbInitializer>();
+    var client = scope.ServiceProvider.GetRequiredService<DiscordSocketClient>();
+
     try 
     {
         await initializer.InitializeAsync();
         Console.WriteLine("[DB] Database initialization completed.");
+        
+        // 注意: ここで DeleteAllGlobalCommandsAsync を呼ぶと、
+        // 以前登録してしまった「重複の原因」であるグローバルコマンドを消去できます。
+        // 一度実行して重複が消えたら、この行は削除するかコメントアウトしてOKです。
+        // await client.Rest.DeleteAllGlobalCommandsAsync();
     }
     catch (Exception ex)
     {
